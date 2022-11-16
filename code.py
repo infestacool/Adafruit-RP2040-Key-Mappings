cff95fccfdfdc05b31bf3a395432cd85495b7099
infestacool <infestacool@users.noreply.github.com>
2022-11-16 08:35:16 -0500

    Update code.py
    Added mission control keys

--- code.py
+++ code.py
@@ -3,17 +3,19 @@ KEY BINDINGS FOR ADAFRUIT RP2040 MACRO PAD
 0 = Zoom Mute Toggle
 1 = Zoom Camrea Toggle
 2 = Sleep Display
-3 = LEFT ARROW
-4 = DOWN ARROW
-5 = RIGHT ARROW
+3 = Mission Control
+4 = Space left
+5 = Space right
 6 = PREVIOUS TRACK
 7 = PAUSE/PLAY
 8 = NEXT TRACK
-9 = COPY
-10 = PASTE
-11 = UNDO
+9 = mute
+10 = Vol Down
+11 = Vol Up
 ENCODER = VOLUME/MUTE
 """
+# https://learn.adafruit.com/macropad-hotkeys?view=all
+
 from adafruit_macropad import MacroPad
 from adafruit_hid.consumer_control_code import ConsumerControlCode
 from rainbowio import colorwheel
@@ -24,19 +26,17 @@ from adafruit_led_animation.animation.rainbowsparkle import RainbowSparkle
 from adafruit_led_animation.sequence import AnimationSequence
 import os
 
-
 macropad = MacroPad()
-
 
 tone = 131
 last_position = 0
 
 # Text of Display "                     "
 text_lines = macropad.display_text(title="        mute/volume >")
-text_lines[0].text = "Zmute   Zcamera sleep"
-text_lines[1].text = "left    down    right"
-text_lines[2].text = "<<    pause/play   >>"
-text_lines[3].text = "copy    paste    undo"
+text_lines[0].text = "Zmute  Zcamera  sleep"
+text_lines[1].text = "^up    ^left   ^right"
+text_lines[2].text = "<<   pause/play    >>"
+text_lines[3].text = "mute    vol-     vol+"
 text_lines.show()
 
 # NeoPixels
@@ -59,7 +59,6 @@ rainbow_sparkle = RainbowSparkle(macropad.pixels, speed=0.4, num_sparkles=4, per
 while True:
     key_event = macropad.keys.events.get()
     #animations.animate()
-    #AnimationSequence(RainbowChase(macropad.pixels, speed=0.2, size=4, spacing=1)).animate()
     #rainbow.animate()
     #rainbow_comet.animate()
     #rainbow_chase.animate()
@@ -76,24 +75,24 @@ while True:
         if key_event.pressed:
             if key_event.key_number == 0: #Toggle Zoom Mic
                 macropad.keyboard.press(macropad.Keycode.SHIFT, macropad.Keycode.COMMAND, macropad.Keycode.A)
-                macropad.pixels.brightness = 0.0
+                #macropad.pixels.brightness = 0.0
                 macropad.keyboard.release_all()
             if key_event.key_number == 1: #Toggle Zoom Camera
                 macropad.keyboard.press(macropad.Keycode.SHIFT, macropad.Keycode.COMMAND, macropad.Keycode.V)
-                macropad.pixels.brightness = 0.0
+                #macropad.pixels.brightness = 0.0
                 macropad.keyboard.release_all()
             if key_event.key_number == 2: #Sleep Display 
                 macropad.pixels.brightness = 0.0
                 macropad.keyboard.press(macropad.Keycode.OPTION, macropad.Keycode.COMMAND, macropad.Keycode.POWER)
                 macropad.keyboard.release_all()
-            if key_event.key_number == 3: #LEFT ARROW
-                macropad.keyboard.press(macropad.Keycode.LEFT_ARROW)
+            if key_event.key_number == 3: #MC
+                macropad.keyboard.press(macropad.Keycode.CONTROL, macropad.Keycode.UP_ARROW)
                 macropad.keyboard.release_all()
             if key_event.key_number == 4: #DOWN ARROW
-                macropad.keyboard.press(macropad.Keycode.DOWN_ARROW)
+                macropad.keyboard.press(macropad.Keycode.CONTROL, macropad.Keycode.LEFT_ARROW)
                 macropad.keyboard.release_all()
             if key_event.key_number == 5: #RIGHT ARROW
-                macropad.keyboard.press(macropad.Keycode.RIGHT_ARROW)
+                macropad.keyboard.press(macropad.Keycode.CONTROL, macropad.Keycode.RIGHT_ARROW)
                 macropad.keyboard.release_all()
             if key_event.key_number == 6: #PREVIOUS TRACK
                 macropad.consumer_control.send(
@@ -107,14 +106,26 @@ while True:
                 macropad.consumer_control.send(
                 macropad.ConsumerControlCode.SCAN_NEXT_TRACK
                 )
-            if key_event.key_number == 9: #COPY
-                macropad.keyboard.press(macropad.Keycode.COMMAND, macropad.Keycode.C)
+            if key_event.key_number == 9: #MUTE
+                #macropad.keyboard.press(macropad.Keycode.COMMAND, macropad.Keycode.C)
+                macropad.consumer_control.send(
+                macropad.ConsumerControlCode.MUTE
+                ) 
+                macropad.pixels.brightness = 0.5
                 macropad.keyboard.release_all()
-            if key_event.key_number == 10: #PASTE
-                macropad.keyboard.press(macropad.Keycode.COMMAND, macropad.Keycode.V)
+            if key_event.key_number == 10: #Vol Down
+                #macropad.keyboard.press(macropad.Keycode.COMMAND, macropad.Keycode.V)
+                macropad.consumer_control.send(
+                macropad.ConsumerControlCode.VOLUME_DECREMENT
+                ) 
+                macropad.pixels.brightness = 0.5
                 macropad.keyboard.release_all()
-            if key_event.key_number == 11: #UNDO
-                macropad.keyboard.press(macropad.Keycode.COMM, macropad.Keycode.Z)
+            if key_event.key_number == 11: #Vol Up
+                #macropad.keyboard.press(macropad.Keycode.COMMAND, macropad.Keycode.Z)
+                macropad.consumer_control.send(
+                macropad.ConsumerControlCode.VOLUME_INCREMENT
+                ) 
+                macropad.pixels.brightness = 0.5
                 macropad.keyboard.release_all()
 
     macropad.encoder_switch_debounced.update()
